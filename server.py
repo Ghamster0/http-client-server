455295076c69226dea1b029a1ffe34171ccdd92f
AiGuangwei <guangwei@sogou-inc.com>
2020-06-12 11:42:30 +0800

    init commit

---
+++ server.py
@@ -0,0 +1,16 @@
+from aiohttp import web
+import asyncio
+
+
+async def handle(request):
+    name = request.match_info.get("name", "Anonymous")
+    text = "Hello, " + name
+    await asyncio.sleep(5)
+    return web.Response(text=text)
+
+
+app = web.Application()
+app.add_routes([web.get("/", handle), web.get("/{name}", handle)])
+
+if __name__ == "__main__":
+    web.run_app(app, port=8080)
